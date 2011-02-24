f817d7863ffc5fd7e5c94903d2fb8df74fe62dde
thomas <thomasalwyndavis@gmail.com>
2011-02-24 18:11:07 +1000

    added function to build all.jst, this shit is the bomb

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 #BLAH MY NIGGER
+rm _attachments/media/templates/all.jst
+find . -name '*.jst' '!' -name 'all.jst' -exec cat '{}' >> _attachments/media/templates/all.jst ';'
+couchapp push http://ryth:abCD--12@ryth.cloudant.com/ryth
