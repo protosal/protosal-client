128032331844a3935723e25ce09c657763e27b10
Ryan Kirkman <ryan@ryankirkman.com>
2011-03-08 11:03:24 +1000

    Made some parts of server.js smaller. Added prototype cascading delete to relationship delete.

--- server.js
+++ server.js
@@ -43,39 +43,6 @@ function get_data(url, method) {
     var request = '';
 }
 
-app.put('/data/:id/:rev?', function(req, res) {
-    var request_url = '/app/' + req.params.id + (req.params.rev ? "?rev=" + req.params.rev : "");
-    app_db_handler(req, res,
-        rCommon.couchdb_request(req, res, request_url, {"method" : req.method}) );
-});
-
-app.post('/data', function(req, res) {
-    var request_url = '/app';
-    app_db_handler(req, res,
-        rCommon.couchdb_request(req, res, request_url, {"method" : req.method}) );
-});
-
-app.post('data/newinstance/:id', function(req, rest) {
-    var request_url = 'app';
-    /* Set the template id for the new record. */
-    req.body.template = req.params.id;
-    /* Set the HTTP request method to COPY so we can leverage CouchDB's
-     * inbuilt functionality.
-     */
-    req.method = 'COPY';
-
-    /* Copy all fees. */
-    if( req.body.type == "section" ) {
-        var url = '/app/_design/section_fee/_view/list_by_parent?key="' + req.params.id + '"';
-        var fee_request = rCommon.couchdb_request(req, res, url,
-            {"method" : req.method});
-    }
-
-    app_db_handler(req, res, rCommon.couchdb_request(req, res, request_url,
-            {"method" : req.method}) );
-});
- 
-
 app.get('/data/:id/:rev?', function(req, res) {
     var request_url = '/app/' + req.params.id + (req.params.rev ? "?rev=" + req.params.rev : "");
     var request = rCommon.couchdb_request(req, res, request_url,
@@ -103,15 +70,52 @@ app.get('/data/:id/:rev?', function(req, res) {
     });
 });
 
-function delete_request(req, res, request_url) {
+app.put('/data/:id/:rev?', function(req, res) {
+    var request_url = '/app/' + req.params.id + (req.params.rev ? "?rev=" + req.params.rev : "");
+    app_db_handler(req, res,
+        rCommon.couchdb_request(req, res, request_url, {"method" : req.method}) );
+});
+
+app.post('/data', function(req, res) {
+    var request_url = '/app';
+    app_db_handler(req, res,
+        rCommon.couchdb_request(req, res, request_url, {"method" : req.method}) );
+});
+
+app.post('/data/newinstance/:id', function(req, rest) {
+    var request_url = 'app';
+    /* Set the template id for the new record. */
+    req.body.template = req.params.id;
+    /* Set the HTTP request method to COPY so we can leverage CouchDB's
+     * inbuilt functionality.
+     */
+    req.method = 'COPY';
+
+    /* Copy all fees. */
+    if( req.body.type == "section" ) {
+        var url = '/app/_design/section_fee/_view/list_by_parent?key="' + req.params.id + '"';
+        var fee_request = rCommon.couchdb_request(req, res, url,
+            {"method" : req.method});
+    }
+
+    app_db_handler(req, res, rCommon.couchdb_request(req, res, request_url,
+            {"method" : req.method}) );
+});
+ 
+
+function delete_request(req, res, request_url, return_value) {
+    if( return_value == null )
+        return_value = true;
     var deleterequest = rCommon.couchdb_request(req, res, request_url,
             {"method" : "DELETE"});
     deleterequest.end();
     
-    deleterequest.on('response', function (responsea) {
-        responsea.on('data', function (data) {
-            res.header('Content-Type', 'application/json');
-            res.send(data);
+    deleterequest.on('response', function (response) {
+        response.on('data', function (data) {
+            if( return_value ) {
+                res.header('Content-Type', 'application/json');
+                res.send(data);
+            }
         })
     });
 }
@@ -142,7 +146,6 @@ app.delete('/data/:id/:rev?', function(req, res) {
     });
 });
 
-
 //Delete the relationship
 app.delete('/delete/:controller/:id/:id2', function(req, res) {
     var request_url = '/app/_design/' + req.params.controller + "/_view/list?key=[\""+ req.params.id + "\",\""+ req.params.id2 +"\"]";
@@ -160,19 +163,34 @@ app.delete('/delete/:controller/:id/:id2', function(req, res) {
 
         response.on('end', function (){
             console.log(data);
+            /* Delete the relationship record. */
             var parsed_data = JSON.parse(data);
             var relationshipid = (parsed_data.rows[0].id);
             var relationshiprev = (parsed_data.rows[0].value._rev);
-            var relationship_url = '/app/' + relationshipid +"?rev=" + relationshiprev;
-            //delete_request(req, res, relationship_url);
-
-            /*
-            var childid = (parsed_data.rows[0].id);
-            var childrev = (parsed_data.rows[0].value._rev);
-            var child_url = '/app/' + relationshipid +"?rev=" + relationshiprev;
-            var child_url = '/app/' + relationshipid +"?rev=" + relationshiprev;
+            var relationship_url = '/app/' + relationshipid + '?rev=' + relationshiprev;
             delete_request(req, res, relationship_url);
-            */
+
+            var childid = (parsed_data.key[1]);
+
+            var child_request_url = '/app/' + childid;
+
+            /* Cascade the delete to the child record. */
+            var child_request = rCommon.couchdb_request(req, res, child_request_url,
+                {"method" : "GET"});
+            child_request.end();
+            child_request.on('response', function(response) {
+                data = '';
+
+                response.on('data', function(chunk) {
+                    data += chunk;
+                });
+
+                response.on('end', function() {
+                    data = JSON.parse(data);
+                    var del_url = '/app/' + childid + '?rev=' + data._rev;
+                    delete_request(req, res, del_url, false);
+                });
+            });
         });
     });
 });
@@ -225,7 +243,7 @@ app.get('/related2/:view/:id', function( req, res ){
         });
         response.on('end', function (){
             
-            keys = _.map(JSON.parse(all_data).rows, function( row ) {
+            keys = _.map(JSON.parse(data).rows, function( row ) {
                 var property = child + '_id';
                 return row.value[property];
             });
@@ -241,14 +259,23 @@ app.get('/related2/:view/:id', function( req, res ){
     });
 });
 
-app.get('/:list_type/:view', function(req, res) {
-    var request_url = '/app/_design/' + req.params.view + "/_view/" + req.params.list_type + "?key=\"" + req.session.username + "\"";
+app.all('/:list_type/:view', function(req, res) {
+    var request_url = "";
+
+    switch(req.method) {
+        case "GET":
+            request_url = '/app/_design/' + req.params.view + "/_view/" + req.params.list_type + "?key=\"" + req.session.username + "\"";
+            break;
+        case "POST":
+            var request_url = '/app/_design/' + req.params.view + "/_view/" + req.params.list_type;
+            break;
+        default:
+            res.header('Content-Type', 'application/json');
+            res.send(JSON.stringify({"error": "Only GET and POST are supported for " + req.params.list_type + "/" + req.params.view}));
+            return;
+    }
+
     generic_list_retrieve(req, res, rCommon.couchdb_request(req, res, request_url,
-            {"method" : req.method}));
-});
-app.post('/:list_type/:view', function(req, res) {
-    var request_url = '/app/_design/' + req.params.view + "/_view/" + req.params.list_type;
-    generic_list_retrieve(req, res, rCommon.couchdb_request(request_url,
             {"method" : req.method}));
 });
 
