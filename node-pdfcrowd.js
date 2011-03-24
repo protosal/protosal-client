b038d58d4fdb03ee6edab253c3c0fadcb4bfb044
Thomas Davis <thomasalwyndavis@gmail.com>
2011-03-24 12:39:14 +1000

    Cleaned up pdf generation on the server. So sexy now.

--- node-pdfcrowd.js
+++ node-pdfcrowd.js
@@ -3,14 +3,6 @@ var fs = require('fs');
 var restler = require('./restler/restler');
 
 exports.generate_pdf = function(username, api_key, src_data, res) {
-    console.log("starting to generate pdf");
-
-    console.log(username);
-    console.log(api_key);
-    console.log(src_data);
-    
-    src_data = decodeURIComponent(src_data).split('+').join(' ');
-
     restler.post('http://pdfcrowd.com/api/pdf/convert/html/', {
         binary: true,
         data: {
@@ -19,15 +11,11 @@ exports.generate_pdf = function(username, api_key, src_data, res) {
             src: src_data
         },
     }).on('complete', function(data, response) {
-        console.log("we are complete, but do we have data?");
-
         /* Actually send the data. Hack it so that the
          * headers and status code are what pdfcrowd
          * responded with.
          */
-        fs.writeFileSync('asdfasdf.pdf', data, 'binary');
         res.send(data, response.headers, response.statusCode);
-
     }).on('error', function(data, response) {
         console.log("ERROR");
         fs.writeFile('data.out', sys.inspect(data));
--- server.js
+++ server.js
@@ -506,7 +506,6 @@ app.post('/user', function(req, res) {
 });
 
 app.post('/pdf', function(req, res) {
-    console.log("trying to generate pdf");
     var username = 'ryankirkman';
     var api_key = 'a4e60e5dc9b00d298fd5f57a6b9c1c3e';
 
