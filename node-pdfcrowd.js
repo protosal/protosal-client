64aa0b611161d417507e6c8e9e4e534c25fab3c2
Ryan Kirkman <ryan@ryankirkman.com>
2011-03-29 17:16:34 +1000

    Fixed posting to /user so that new attachments can be uploaded. Implemented prototype pdf email sending.

--- node-pdfcrowd.js
+++ node-pdfcrowd.js
@@ -1,24 +1,15 @@
-var sys = require('sys');
-var fs = require('fs');
 var restler = require('./restler/restler');
 
-exports.generate_pdf = function(username, api_key, src_data, res) {
-    restler.post('http://pdfcrowd.com/api/pdf/convert/html/', {
+var username = 'ryankirkman';
+var api_key = 'a4e60e5dc9b00d298fd5f57a6b9c1c3e';
+
+exports.generate_pdf = function(src_data) {
+    return restler.post('http://pdfcrowd.com/api/pdf/convert/html/', {
         binary: true,
         data: {
             username: username,
             key: api_key,
             src: src_data
         },
-    }).on('complete', function(data, response) {
-        /* Actually send the data. Hack it so that the
-         * headers and status code are what pdfcrowd
-         * responded with.
-         */
-        res.send(data, response.headers, response.statusCode);
-    }).on('error', function(data, response) {
-        console.log("ERROR");
-        fs.writeFile('data.out', sys.inspect(data));
-        fs.writeFile('response.out', sys.inspect(response));
     });
 }
