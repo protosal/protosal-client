59fe7705582bc48eb3423d347ebdf04c3ca54803
samot <thomasalwyndavis@gmail.com>
2011-04-05 18:22:26 +1000

    Added index.js to docs

--- dev_build.sh
+++ dev_build.sh
@@ -3,5 +3,6 @@ rm public/media/templates/all.js
 find public/media/templates -name '*.html' '!' -name 'all.html' -exec cat '{}' >> public/media/templates/all.html ';'
 find public/media/templates -name '*.js' '!' -name 'all.js' -exec cat '{}' >> public/media/templates/all.js ';'
 
-docco public/media/templates/**/*.js
-ln -s docs public/docs
+docco public/media/templates/**/*.js public/index.js
+cd public
+ln -s ../docs docs
