d6f47b7d580f6c69741d726e3df0238a2ca08a4e
Ryan Kirkman <ryan@ryankirkman.com>
2011-04-06 00:20:25 +1000

    Split mashing of html and js files into its own shell script.

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-rm public/media/templates/all.html
-rm public/media/templates/all.js
-find public/media/templates -name '*.html' '!' -name 'all.html' -exec cat '{}' >> public/media/templates/all.html ';'
-find public/media/templates -name '*.js' '!' -name 'all.js' -exec cat '{}' >> public/media/templates/all.js ';'
+./mash.sh
 
 node server.js
--- dev_build.sh
+++ dev_build.sh
@@ -1,7 +1,4 @@
-rm public/media/templates/all.html
-rm public/media/templates/all.js
-find public/media/templates -name '*.html' '!' -name 'all.html' -exec cat '{}' >> public/media/templates/all.html ';'
-find public/media/templates -name '*.js' '!' -name 'all.js' -exec cat '{}' >> public/media/templates/all.js ';'
+./mash.sh
 
 docco public/media/templates/**/*.js public/index.js
 cd public
