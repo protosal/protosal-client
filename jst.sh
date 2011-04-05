a4a4fee73f874a227570abc8e763e628d6c09b41
samot <thomasalwyndavis@gmail.com>
2011-04-05 18:05:48 +1000

    Cleaned up and renamed jst templates

--- jst.sh
+++ jst.sh
@@ -1,6 +1,6 @@
-rm public/media/templates/all.jst
+rm public/media/templates/all.html
 rm public/media/templates/all.js
-find public/media/templates -name '*.jst' '!' -name 'all.jst' -exec cat '{}' >> public/media/templates/all.jst ';'
+find public/media/templates -name '*.html' '!' -name 'all.html' -exec cat '{}' >> public/media/templates/all.html ';'
 find public/media/templates -name '*.js' '!' -name 'all.js' -exec cat '{}' >> public/media/templates/all.js ';'
 
 docco public/media/templates/**/*.js
