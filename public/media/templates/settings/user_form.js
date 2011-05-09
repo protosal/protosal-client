1a21ec5927a1d0916adcdfdc4566f84979480c59
Thomas Davis <thomasalwyndavis@gmail.com>
2011-05-10 05:18:56 +1000

    asd

--- public/media/templates/settings/user_form.js
+++ public/media/templates/settings/user_form.js
@@ -1,6 +1,7 @@
 user_form_view = Backbone.View.extend({
     initialize: function( options ) {
         this.renderView("#user_formc", "#user_form_template", options);
+        clicky.log("i am settings page");
         $('.user_save').unbind('click');
 
         $('.user_save').click(function() {
